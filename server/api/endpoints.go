ac4844a7b659285f1d6320587f75880a3482bd00
nullsurface <66500106+nullsurface@users.noreply.github.com>
2025-01-18 14:05:39 -0800

    Finished get inv structure

--- server/api/endpoints.go
+++ server/api/endpoints.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
   "database/sql"
 	"net/http"
+  "resourceflow/process"
 )
 
 func Health(c *gin.Context) {
@@ -12,6 +13,7 @@ func Health(c *gin.Context) {
 
 func GetInventory(db *sql.DB) func(c *gin.Context) {
   return func(c *gin.Context) {
-    
+    query := c.Param("query")
+    process.GetItems(db, query)
   }
 }
