8a4e42bd7f90260f80217e244a70940aad28c935
nullsurface <66500106+nullsurface@users.noreply.github.com>
2025-01-18 15:52:36 -0800

    Finished Dockerfile

--- server/process/inventory.go
+++ server/process/inventory.go
@@ -2,12 +2,15 @@ package process
 
 import (
 	"database/sql"
-  "resourceflow/model"
+	"resourceflow/model"
 )
 
 func GetItems(db *sql.DB, query string) []model.Item {
+	var items []model.Item
+
+	return items
 }
 
-func AddItems(db *sql.DB, []model.Item) {
+func AddItems(db *sql.DB, items []model.Item) {
 
 }
