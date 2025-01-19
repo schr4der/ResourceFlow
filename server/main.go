e51cf47b8e859066bedf727026028f77951ebdc1
nullsurface <66500106+nullsurface@users.noreply.github.com>
2025-01-18 17:38:20 -0800

    docker compose fixed

--- server/main.go
+++ server/main.go
@@ -14,7 +14,7 @@ var dbFile = os.Getenv("DATABASE_FILE")
 
 func main() {
 	// Connect to the database
-	db, err := sql.Open("sqlite3", "example.db")
+	db, err := sql.Open("sqlite3", "db/example.db")
 	if err != nil {
 		panic(err)
 	}
